9c9c8c66ad9c1ac8e16661d11540cb553f715992
Zeshan Rasul <zeshanrasul@gmail.com>
2020-11-21 16:46:33 +0000

    Revert window exception test

--- Hazel/src/Platform/Windows/WindowsWindow.cpp
+++ Hazel/src/Platform/Windows/WindowsWindow.cpp
@@ -101,7 +101,7 @@ namespace Hazel {
 		const wchar_t* pWindowName = L"Hazel Engine";
 
 		HWND m_Hwnd = CreateWindowEx(
-			0, L"HELLO",
+			0, WindowClass::GetName(),
 			pWindowName,
 			WS_CAPTION | WS_MINIMIZEBOX | WS_MAXIMIZEBOX | WS_SYSMENU,
 			CW_USEDEFAULT, CW_USEDEFAULT, wr.right - wr.left, wr.bottom - wr.top,
